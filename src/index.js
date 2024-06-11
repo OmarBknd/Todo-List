e8a781b3c164226c01c9963203becbcab30f9e7c
OmarBknd <omar.bknd@gmail.com>
2024-06-11 21:00:17 +0100

    '.'

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import tasks from "./task";
 import projects from "./project";
-//
-import './style.css';
 import tasksDom from "./tasksDom";
 import projectsDom from "./projectsDom";
+
+import './style.css';
 
 
 console.log('index page')
 
-tasks()
-projects()
-projectsDom()
-tasksDom()
+//tasks()
+//projects()
+//projectsDom()
+//tasksDom()
